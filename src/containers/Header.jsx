725bbfe9178aa4e72ab8713f5f41cc19b1e92783
Visinho <Elvisvisinho@rocketmail.com>
2023-02-16 10:43:15 +0100

    Completed the simple redux application without redux toolkit to demonstrate state management

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
+  <Link to ={"/"}>
     <div className='ui fixed menu'>
       <div className='ui container center'>
         <h1>Visinho Shop</h1>
       </div>
     </div>
+  </Link>
   )
 }
 
